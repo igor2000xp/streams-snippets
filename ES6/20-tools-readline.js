fa4a319aad1afc5c718046e1ea38a4f7d3b80c81
igor2000xp <igorring@gmail.com>
2022-06-13 09:51:12 +0300

    feat: my syntax es6 20-tools-readline.jl

---
+++ ES6/20-tools-readline.js
@@ -0,0 +1,37 @@
+// const { createInterface } = require('readline');
+import { createInterface } from 'readline';
+
+const questionnaire = createInterface(process.stdin, process.stdout);
+
+const questions = [
+    'What is your name?',
+    'How old are you?',
+    'What are your hobbies?',
+    'Do you like Node.js?',
+    'Where were you last night?'
+];
+
+const answers = [];
+
+const questionsPromises = questions.map((question) => () => {
+  return new Promise((res, _) => {
+    questionnaire.question(`${question}\n`, (answer) => {
+      answers.push(answer);
+      res();
+    });
+  })
+});
+
+questionnaire.on('close', () => {
+    console.log(answers);
+});
+
+const askQuestions = async () => {
+    for (const question of questionsPromises) {
+        await question();
+    }
+
+    questionnaire.close();
+};
+
+await askQuestions();
